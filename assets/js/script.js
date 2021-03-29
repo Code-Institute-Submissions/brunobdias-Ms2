0fc923d706320e338d8f33c2fe0d279cd6166a12
brunobdias <brunobdias@live.com>
2021-03-29 13:47:21 +0000

    changed scrollFunction from js to jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,18 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.opacity = "1";
-        document.getElementById("navbar").style.padding = "0.5rem 0.5rem";
-        document.getElementById("brand").style.fontSize = "1.5rem";
-        document.getElementById("brand").style.weight = "bold";
-        document.getElementById("logo").src = "assets/images/navlogo.png";
+        $("#navbar").css("opacity", "1");
+        $("#navbar").css("padding", "0.5rem 0.5rem");
+        $("#brand").css("fontSize", "1.5rem");
+        $("#brand").css("weight", "bold");
+        $("#logo").css("src", "assets/images/navlogo.png");
 
-        $("navbar").css.opacity()
     } else {
-        document.getElementById("navbar").style.opacity = "0.85";
-        document.getElementById("navbar").style.padding = "2rem 0.5rem";
-        document.getElementById("brand").style.fontSize = "2rem";
-        document.getElementById("brand").style.weight = "bold";
-        document.getElementById("logo").src = "assets/images/navlogo-lg.png";
+        $("#navbar").css("opacity", "0.85");
+        $("#navbar").css("padding", "2rem 0.5rem");
+        $("#brand").css("fontSize", "2rem");
+        $("#brand").css("weight", "bold");
+        $("#logo").css("src", "assets/images/navlogo-lg.png");
     }
 }
 
