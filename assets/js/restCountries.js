ea113498bff76da1c121715219d67ec05ce55f92
brunobdias <brunobdias@live.com>
2021-04-01 03:26:25 +0000

    Fetching data test of RestCountries api, create restCountries.js file

---
+++ assets/js/restCountries.js
@@ -0,0 +1,103 @@
+//https://restcountries.eu/rest/v2/alpha/?codes=ita;mex;per;bra;chn;jor;ind
+//fetch('https://restcountries.eu/rest/v2/alpha/cn')
+fetch('https://restcountries.eu/rest/v2/alpha/cn')
+    .then(res => res.json())
+    .then(data => initialize(data))
+    .catch(err => console.log('Error:', err.message));
+
+
+function initialize({
+    name,
+    capital,
+    callingCodes,
+    population,
+    currencies,
+    region,
+    latlng,
+    timezones,
+    borders,
+    flag
+}) {
+    console.log({
+        name,
+        capital,
+        callingCodes,
+        population,
+        currencies,
+        region,
+        latlng,
+        timezones,
+        borders,
+        flag
+    });
+    
+    $(".country-info").html(`<div class"info">
+                    <p> Name: ${name} <p>
+                    <p> Capital: ${capital} </p>
+                    <p> Lat. Long.: ${latlng} <p>
+                    <p> Calling Code: ${callingCodes} <p>
+                    <p> Timezone: ${timezones} <p>
+                    <p> Borders: ${borders} <p>
+                    <p> Currencie: ${currencies} <p>
+                    <p> Population: ${population} <p>
+                    <p> Flag: ${flag} <p>
+                </div>`);
+
+}
+
+/*
+
+https://restcountries.eu/rest/v2/alpha/col
+[{
+"name": "Colombia",
+"topLevelDomain": [".co"],
+"alpha2Code": "CO",
+"alpha3Code": "COL",
+"callingCodes": ["57"],
+"capital": "Bogotá",
+"altSpellings": ["CO", "Republic of Colombia", "República de Colombia"],
+"region": "Americas",
+"subregion": "South America",
+"population": 48759958,
+"latlng": [4.0, -72.0],
+"demonym": "Colombian",
+"area": 1141748.0,
+"gini": 55.9,
+"timezones": ["UTC-05:00"],
+"borders": ["BRA", "ECU", "PAN", "PER", "VEN"],
+"nativeName": "Colombia",
+"numericCode": "170",
+"currencies": [{
+    "code": "COP",
+    "name": "Colombian peso",
+    "symbol": "$"
+}],
+"languages": [{
+    "iso639_1": "es",
+    "iso639_2": "spa",
+    "name": "Spanish",
+    "nativeName": "Español"
+}],
+"translations": {
+    "de": "Kolumbien",
+    "es": "Colombia",
+    "fr": "Colombie",
+    "ja": "コロンビア",
+    "it": "Colombia",
+    "br": "Colômbia",
+    "pt": "Colômbia"
+},
+"flag": "https://restcountries.eu/data/col.svg",
+"regionalBlocs": [{
+    "acronym": "PA",
+    "name": "Pacific Alliance",
+    "otherAcronyms": [],
+    "otherNames": ["Alianza del Pacífico"]
+}, {
+    "acronym": "USAN",
+    "name": "Union of South American Nations",
+    "otherAcronyms": ["UNASUR", "UNASUL", "UZAN"],
+    "otherNames": ["Unión de Naciones Suramericanas", "União de Nações Sul-Americanas", "Unie van Zuid-Amerikaanse Naties", "South American Union"]
+}],
+"cioc": "COL"
+}]*/
