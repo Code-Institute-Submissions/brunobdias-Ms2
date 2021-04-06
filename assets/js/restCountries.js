bd677bb00b15de4271e5aa87dc0bc007b30dcd7f
brunobdias <brunobdias@live.com>
2021-04-06 00:48:05 +0000

    Improved showing flag, and some descriptions

--- assets/js/restCountries.js
+++ assets/js/restCountries.js
@@ -24,37 +24,27 @@ function fetchCountryData(placeCountry) {
         borders,
         flag
     }) {
-        console.log({
-            name,
-            capital,
-            callingCodes,
-            population,
-            currencies,
-            region,
-            subregion,
-            latlng,
-            timezones,
-            borders,
-            flag
-        });
-                
+                        
         population = formatNumber(population);
         currencies = setCurrencie(currencies);
-
+        
+        
+        //clearfix CREDIT https://getbootstrap.com/docs/5.0/layout/columns/
         $(".country-info").html(`
-                <div class="info">
+                <div class="info clearfix">
                     <div class="flag">
-                        <img src= ${flag} alt= ${name}/>
+                        <img src= ${flag} class="col-md-6 float-md-end mb-3 ms-md-3"
+                         alt= ${name}/>
                     </div>
                     <p> <strong>Country:</strong> ${name} <p>
                     <p> <strong>Capital:</strong> ${capital} </p>
-                    <p> <strong>Latitude & Longitude:</strong> <br>${latlng}<p>
+                    <p> <strong>Latitude & Longitude:</strong> ${latlng}<p>
                     <p> <strong>Region:</strong> ${region} </p>
                     <p> <strong>Subregion:</strong> ${subregion} </p>
                     <p> <strong>Currency:</strong> ${currencies}</p>
                     <p> <strong>Calling Code:</strong> +${callingCodes} <p>
                     <p> <strong>Time Zone:</strong> ${timezones} <p>
-                    <p> <strong>Borders:</strong> <br>${borders} <p>
+                    <p> <strong>Borders:</strong> ${borders} <p>
                     <p> <strong>Population:</strong> ${population} <p>
                     <p>Font:
                         <a class="country-font-link" href="https://restcountries.eu/rest/v2/alpha/${placeCountry}" target="_blank"> Rest Countries EU <i class="fas fa-external-link-alt" aria-hidden="true"></i></a>
@@ -63,69 +53,11 @@ function fetchCountryData(placeCountry) {
     }
 
     function setCurrencie(currenciesArray){     
-        return currenciesArray[0].name + " " +  currenciesArray[0].symbol;
+        return currenciesArray[0].name + " " + currenciesArray[0].symbol;
     }
 
+    //CREDIT https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
     function formatNumber(pNumber){
-          return new Intl.NumberFormat().format(pNumber);
-          //CREDIT https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+          return new Intl.NumberFormat().format(pNumber);          
     }
 }
-
-
-/*
-
-https://restcountries.eu/rest/v2/alpha/col
-[{
-"name": "Colombia",
-"topLevelDomain": [".co"],
-"alpha2Code": "CO",
-"alpha3Code": "COL",
-"callingCodes": ["57"],
-"capital": "Bogotá",
-"altSpellings": ["CO", "Republic of Colombia", "República de Colombia"],
-"region": "Americas",
-"subregion": "South America",
-"population": 48759958,
-"latlng": [4.0, -72.0],
-"demonym": "Colombian",
-"area": 1141748.0,
-"gini": 55.9,
-"timezones": ["UTC-05:00"],
-"borders": ["BRA", "ECU", "PAN", "PER", "VEN"],
-"nativeName": "Colombia",
-"numericCode": "170",
-"currencies": [{
-    "code": "COP",
-    "name": "Colombian peso",
-    "symbol": "$"
-}],
-"languages": [{
-    "iso639_1": "es",
-    "iso639_2": "spa",
-    "name": "Spanish",
-    "nativeName": "Español"
-}],
-"translations": {
-    "de": "Kolumbien",
-    "es": "Colombia",
-    "fr": "Colombie",
-    "ja": "コロンビア",
-    "it": "Colombia",
-    "br": "Colômbia",
-    "pt": "Colômbia"
-},
-"flag": "https://restcountries.eu/data/col.svg",
-"regionalBlocs": [{
-    "acronym": "PA",
-    "name": "Pacific Alliance",
-    "otherAcronyms": [],
-    "otherNames": ["Alianza del Pacífico"]
-}, {
-    "acronym": "USAN",
-    "name": "Union of South American Nations",
-    "otherAcronyms": ["UNASUR", "UNASUL", "UZAN"],
-    "otherNames": ["Unión de Naciones Suramericanas", "União de Nações Sul-Americanas", "Unie van Zuid-Amerikaanse Naties", "South American Union"]
-}],
-"cioc": "COL"
-}]*/
