7853e11e490ff407f9e92a933c2a828352068f18
brunobdias <brunobdias@live.com>
2021-04-17 21:01:43 +0000

    Add Clear form after Sent email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -11,7 +11,10 @@ function sendEmail(contactForm) {
     .send("service_f04f4oq", "template_geli2d5", templateParams)
     .then(
       function (response) {
-        alert("SUCCESS!", response.status, response.text);
+        //Credit to https://stackoverflow.com/questions/6653556/jquery-javascript-function-to-clear-all-the-fields-of-a-form
+        $("#contactForm").trigger("reset");
+        //End Credit
+        alert("Email Sent!", response.status, response.text);
       },
       function (error) {
         alert("FAILED...", error)
